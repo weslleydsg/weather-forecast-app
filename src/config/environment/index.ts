61cd5942c730ba5975b25bd2f1d727d39edf4e50
Weslley da Silva Gomes <weslleydsg@outlook.com>
2022-02-16 19:50:18 -0300

    feat: setup api hooks

--- src/config/environment/index.ts
+++ src/config/environment/index.ts
@@ -4,6 +4,8 @@ import { Environment } from '~/types';
 const environment: Environment = {
   placesApiUrl: Config.PLACES_API_URL,
   weatherApiUrl: Config.WEATHER_API_URL,
+  placesApiKey: Config.PLACES_API_KEY,
+  weatherApiKey: Config.WEATHER_API_KEY,
 };
 
 export default environment;
